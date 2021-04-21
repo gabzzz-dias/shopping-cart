428ec38a681cab6f8a78d061b8a756c5c995d985
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-04-21 18:29:47 -0300

    request 3

--- script.js
+++ script.js
@@ -30,11 +30,18 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+// Para resolver o requisito 2 obtive ajuda do meu colega de turma Leandro Reis, consultando seu PR para entender a lógica por trás desse requisito. src: https://github.com/tryber/sd-010-b-project-shopping-cart/pull/6
+
+function cartItemClickListener(event) {
+  const removeItem = event.target;
+  removeItem.parentNode.removeChild(removeItem);
+}
+
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  // li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
@@ -50,8 +57,6 @@ async function addToCart(event) {
   document.querySelector('.cart__items').appendChild(add);
 }
 
-// Para resolver o requisito 2 obtive ajuda do meu colega de turma Leandro Reis, consultando seu PR para entender a lógica por trás desse requisito. src: https://github.com/tryber/sd-010-b-project-shopping-cart/pull/6
-
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -65,10 +70,6 @@ function createProductItemElement({ sku, name, image }) {
 
   return section;
 }
-
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
 
 const showItems = async () => {
   const itemData = await fetchItems();
